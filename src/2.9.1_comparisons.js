33a97879ab61720ebe3b7150e48fe8b850297d43
unknown <kaution.burrowes@gmail.com>
2023-06-24 08:42:37 +1200

    Update

---
+++ src/2.9.1_comparisons.js
@@ -0,0 +1,8 @@
+5 > 4 // true
+"apple" > "pineapple" // false
+"2" > "12"  // false x, true bc it doesn't convert to integers
+            // it is a dictionary convert and 2 is greater than 1
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
